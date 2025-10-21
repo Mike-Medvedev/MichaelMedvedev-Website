bdb70dc66bef9cd5e20aeebe5715301f5faee442
Michael Medvedev <mmedvedev20@gmail.com>
2025-10-21 19:54:43 -0400

    wireframing screens

--- client/pages/home.js
+++ client/pages/home.js
@@ -2,10 +2,42 @@
  * Todo: Implement home screen
  */
 
+function Avatar(){
+const imgContainer = document.createElement("div")
+imgContainer.className = "avatar-image"
+
+const avatar = document.createElement("img")
+avatar.src = "assets/mike-holding-guitar.png"
+
+imgContainer.appendChild(avatar)
+return imgContainer;
+}
+
+
 export default function Home(){
     const container = document.createElement("div")
-    const text = document.createElement("p");
-    text.innerText = "Home"
-    container.appendChild(text)
+    container.className = "main-container"
+
+    const avatar = Avatar();
+
+    const name = document.createElement("h1")
+    name.innerText = "Michael Medvedev"
+    name.className = "author-name"
+
+    const job = document.createElement("h4")
+    job.innerText = "Software Engineer"
+    job.className = "author-job"
+
+    const p1 = document.createElement("p")
+    p1.innerText = "I'm an entrepreneur, engineer, musician, and snowboarder. I am the founder of Gilded Guitars and more recently, infatuated with programming. I build with the goal of impacting millions for the better."
+
+    const p2 = document.createElement("p")
+    p2.innerText = "I'm open sourcing my life, tracking every activity I do, to visualize passion."
+
+    const p3 = document.createElement("p")
+    p3.innerText = '"Little by little, one travels far." — J.R.R. Tolkien'
+
+    container.append(avatar, name, job, p1, p2, p3);
     return container
 }
+
--- client/pages/programming.js
+++ client/pages/programming.js
@@ -1,10 +1,87 @@
 /**
  * Todo: Implement programming screen
  */
+
+
+function TechStack(){
+    const techstackContainer = document.createElement("div")
+    techstackContainer.className = "techstack-container"
+
+    const text = document.createElement("h4");
+    text.innerText = "Technology Stack";
+    text.className = "techstack-text"
+
+
+    techstackContainer.appendChild(text)
+
+    const listContainer = document.createElement("div");
+    listContainer.className="techstack-list-container"
+
+    const techList = ["React", "Angular", "Express", "JavaScript", "Python", "FastAPI"];
+    const nodeList = techList.map((t) => {
+        const techContainer = document.createElement("div");
+        techContainer.className = "techContainer";
+
+        const text = document.createElement("p")
+        text.innerText = t
+
+        techContainer.appendChild(text)
+
+        return techContainer;
+    })
+    
+    listContainer.append(...nodeList)
+
+    techstackContainer.appendChild(listContainer)
+
+    return techstackContainer
+
+}
+
+function Projects(){
+    const projectContainer = document.createElement("div");
+    projectContainer.className = "project-list-container";
+
+    const projectHeader = document.createElement("h4");
+    projectHeader.className = "project-header";
+    projectHeader.innerText = "Projects"
+
+    const projectItem = document.createElement("div");
+    projectItem.className = "project-item";
+
+    const projectItemHeader = document.createElement("h4");
+    projectItemHeader.className = "project-item-header"
+    projectItemHeader.innerText = "AI Task Manager"
+
+    const projectItemDescription = document.createElement("p");
+    projectItemDescription.className = "project-chips-container";
+    projectItemDescription.innerText = "A smart task management system with AI-powered prioritization and scheduling recommendations.";
+    
+    const projectChipsContainer = document.createElement("div");
+    projectChipsContainer.className = "project-chips-container"
+
+    const projectTech = ["React", "TypeScript", "FastAPI", "PostgreSQl", "OpenAI"];
+    projectTech.map((c) => {
+        const chip = document.createElement("span");
+        chip.className = "chip"
+        
+        const text = document.createTextNode(c)
+        chip.appendChild(text)
+
+        return chip;
+    })
+    projectChipsContainer.append(...projectTech)
+    projectItem.append(projectItemHeader, projectItemDescription, projectChipsContainer)
+    projectContainer.append(projectHeader, projectItem )
+    
+    return projectContainer
+}
+
 export default function Programming(){
     const container = document.createElement("div")
-    const text = document.createElement("p");
-    text.innerText = "Programming"
-    container.appendChild(text)
+    const techStack = TechStack() 
+    const projects = Projects()
+    container.appendChild(techStack)
+    container.appendChild(projects)
     return container
 }
