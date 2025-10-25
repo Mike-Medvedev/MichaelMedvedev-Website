e3c9e0abc0b2df10bf874c82d7a80a5a9744b586
Michael Medvedev <mmedvedev20@gmail.com>
2025-10-25 18:07:30 -0400

    working on cell data

--- client/main.js
+++ client/main.js
@@ -22,27 +22,43 @@ loadHeatMap().then(() => {
     })
     heatmap.addEventListener("click", async (event) => {
         const cell = event.target.closest("td");
-        const activityOverview = document.querySelector(".activity-overview")
         if(!cell) return;
-        if(cell.getAttribute("selected")){ 
+        //when a cell is selected, remove other selected cells and their content.
+        // fetch data for selected cell, set current cell attribute selected to true, add dom elements
+
+
+        const previousSelectedCells = heatmap.querySelectorAll("td[selected='true']");
+        const activityOverview = document.querySelector(".activity-overview")
+        const activityContainer = document.querySelector(".activity")
+
+        if(cell.getAttribute("selected")){ //when selecting previously selected cell
             cell.removeAttribute("selected");
             activityOverview.style.display = "none";
+            const pElements = activityContainer.querySelectorAll("p");
+            pElements.forEach(p => p.remove());
             return;
         }
-        cell.setAttribute("selected", "true");
-        activityOverview.style.display = "block";
-        const cellDate = cell.getAttribute("data-date");
-        const response = await fetch(`http://localhost:3000/activity?selected-day=${cellDate}`);
-        const result = await response.json();
-        console.log(result);
-        const activityContainer = document.querySelector(".activity")
-        for(let activity of result.activities){
-            const p = document.createElement("p")
-            p.innerText = activity;
-            p.classList.add("mr-1");
-            activityContainer.appendChild(p)
+        else{ //when selecting different cell
+            if(previousSelectedCells.length > 0){
+                previousSelectedCells.forEach(cell => {
+                    cell.removeAttribute("selected");
+                })
+            }
+
+            cell.setAttribute("selected", "true");
+            activityOverview.style.display = "block";
+            const cellDate = cell.getAttribute("data-date");
+            const response = await fetch(`http://localhost:3000/activity?selected-day=${cellDate}`);
+            const result = await response.json();
+            console.log(result);
+            
+            for(let activity of result.activities){
+                const p = document.createElement("p")
+                p.innerText = activity;
+                p.classList.add("mr-1");
+                activityContainer.appendChild(p)
+            }
         }
-        
     })
     heatmap.addEventListener("mouseout", (event) => {
         const cell = event.target.closest("td");
