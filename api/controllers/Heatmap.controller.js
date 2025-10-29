4be32466ead626288ff76900da220abf34a2bf59
Michael Medvedev <mmedvedev20@gmail.com>
2025-10-28 23:42:35 -0400

    refactor heatmap

--- api/controllers/Heatmap.controller.js
+++ api/controllers/Heatmap.controller.js
@@ -1,16 +1,11 @@
 import express from "express"
+import renderHeatMap from "../heatmap.js"
 
 const HeatmapRouter = express.Router();
 
 HeatmapRouter.get("/", (req, res) => {
-    const heatmapHtml = functions.createTable(database);
+    const heatmapHtml = renderHeatMap()
     res.status(200).send(heatmapHtml);
-    // get current day and a year before today
-    // get activites for each day in that range 
-    // generate table rows for each day (sunday 2024 - sunday 2025, saturday 2024 - saturday 2025)
-    // each column is one week, so iterator over each week, and for each week, get a day
-    // one uyear of milliseconds = 1000 * 60 * 60 * 24 * 365
-    // i can get the last year by making a new date and doing .setFullyear()
 })
 
 
