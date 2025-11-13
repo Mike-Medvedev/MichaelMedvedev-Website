8fa19a4edab9757d77af3b137e181a39593a38df
Michael Medvedev <mmedvedev20@gmail.com>
2025-11-13 11:41:53 -0500

    wrote node scripts and converted pics towebp and resized them

--- client/main.js
+++ client/main.js
@@ -2,10 +2,8 @@ import router from "./router.js"
 import "./web-components/custom-tooltip.js"
 import "./config.js"
 import Heatmap from "./components/heatmap.js"
-import MobileMenu from "./components/mobile-menu.js"
 import HeatMapYearButtons from "./components/heatmap-year-button.js"
 
 router.setupNavigation();
 Heatmap().mount();
 HeatMapYearButtons().createComponent()
-MobileMenu().mount()
---
+++ client/utils/media.utils.js
@@ -0,0 +1,38 @@
+import { readdirSync, renameSync } from "node:fs"
+import path from "node:path"
+import { spawn } from "node:child_process"
+
+function replaceExt(str, ext){
+    const newStr = str.replace(/\.[^.]+$/, ext); // file.jpg -> file.webp
+    return newStr;
+}
+
+function convertImageToWebp(){
+    //find all jpg/jpeg/pngs
+    const cwd = process.cwd();
+    const assetsRelativePath = "client/assets";
+    const assetsDir = path.join(cwd, assetsRelativePath);
+    const assetContents = readdirSync(assetsDir);
+    const filesToConvert = assetContents.filter(file => file.endsWith(".jpg") || file.endsWith(".jpeg") || file.endsWith(".png") || file.endsWith(".webp"));
+
+    for(const file of filesToConvert){
+        const subprocess = spawn("ffmpeg", ["-y", "-i", file, "-vf", "scale=800:-1", "-q:v", "80", "temp" + replaceExt(file, ".webp")], { cwd: assetsDir });
+        subprocess.stderr.on(("data"), (data) => console.log(data.toString()));
+    }
+}
+
+function removeTemp(){
+    const cwd = process.cwd();
+    const assetRelativePath = "client/assets";
+    const fullPath = path.join(cwd, assetRelativePath);
+
+    const contents = readdirSync(fullPath);
+    const temppaths = contents.filter(file => file.startsWith("temp"));
+
+    temppaths.forEach(file => {
+        const oldPath = path.join(fullPath, file);
+        const newPath = path.join(fullPath, file.slice(4));
+        renameSync(oldPath, newPath);
+    });
+}
+
